a6271b57f74d22601a93e534f9422693e6ff5d45
Jsilvermist <jsilvermist@silverlinkz.net>
2018-06-17 16:43:07 -0400

    Fix indenting and remove unneeded escapes

--- sl-gallery-slideshow.js
+++ sl-gallery-slideshow.js
@@ -10,253 +10,253 @@ import './sl-gallery-icons.js';
 class SLGallerySlideshow extends PolymerElement {
   static get template() {
     return html`
-    <style>
-      :host {
-        display: flex;
-        background: var(--sl-gallery-slideshow-background, rgba(0, 0, 0, 0.9));
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100vh;
-        width: 100vw;
-        z-index: 1;
-        align-items: center;
-        justify-content: center;
-        overflow: hidden;
-        outline: none;
-      }
-
-      :host([hidden]) {
-        display: none !important;
-      }
-
-      paper-spinner-lite {
-        position: absolute;
-        width: 42px;
-        height: 42px;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-        z-index: 1;
-        --paper-spinner-color: var(--sl-gallery-accent-color, #00AEC5);
-        --paper-spinner-stroke-width: 4px;
-      }
-
-      .image-container {
-        display: block;
-      }
-
-      .image-container iron-image {
-        width: 100vw;
-        height: 100vh;
-      }
-
-      /* Overlay Animations */
-
-      #overlay app-toolbar,
-      #overlay #clickBlocks > div,
-      paper-icon-button#closeSlideshow {
-        will-change: transform, opacity;
-        transition: top 0.35s ease, bottom 0.35s ease, right 0.35s ease,
-                    opacity 0.35s ease, visibility 0.35s ease;
-      }
-
-      /* Overlay Toolbar */
-
-      #overlay app-toolbar {
-        z-index: 2;
-        position: fixed;
-        left: 0;
-        right: 0;
-        background-color: rgba(0, 0, 0, 0.65);
-        color: white;
-      }
-
-      #overlay app-toolbar.header {
-        top: -64px;
-        height: 64px;
-        visibility: hidden;
-      }
-
-      #overlay app-toolbar.footer {
-        height: auto;
-        min-height: 64px;
-        bottom: -64px;
-        opacity: 0;
-        visibility: hidden;
-        text-align: center;
-        --app-toolbar-font-size: 14px;
-        color: #ddd;
-      }
-
-      #overlay[toolbar-visible] app-toolbar.header {
-        top: 0;
-        visibility: visible;
-      }
-
-      #overlay[toolbar-visible] app-toolbar.footer[has-caption] {
-        bottom: 0;
-        opacity: 1;
-        visibility: visible;
-      }
-
-      /* Overlay Buttons */
-
-      /* @media (hover: hover) in CSS4 */
-      #overlay[hover-enabled] paper-icon-button:hover {
-        background: rgba(99, 99, 99, 0.3);
-        border: 1px solid rgba(88, 88, 88, 0.66);
-      }
-
-      #overlay[hover-enabled] paper-icon-button:active {
-        background: rgba(0, 0, 0, 0.3);
-        border: 0;
-      }
-
-      #overlay > paper-icon-button,
-      #overlay :not(app-toolbar) paper-icon-button {
-        color: #fff;
-        z-index: 2;
-        border-radius: 50%;
-        background: rgba(0, 0, 0, 0.3);
-        width: 48px;
-        height: 48px;
-      }
-
-      #overlay paper-icon-button#closeSlideshow {
-        position: absolute;
-        top: 8px;
-        right: 8px;
-        visibility: visible;
-      }
-
-      #overlay[toolbar-visible] paper-icon-button#closeSlideshow {
-        /* top: -48px; */
-        right: -48px;
-        visibility: hidden;
-      }
-
-      #clickBlocks #previousImageBlock paper-icon-button {
-        position: absolute;
-        left: 8px;
-        top: 50%;
-        transform: translateY(-50%);
-      }
-
-      #clickBlocks #nextImageBlock paper-icon-button {
-        position: absolute;
-        right: 8px;
-        top: 50%;
-        transform: translateY(-50%);
-      }
-
-      /* Overlay Click Blocks */
-
-      #overlay #clickBlocks > div {
-        z-index: 1;
-        position: fixed;
-        top: 0;
-        bottom: 0;
-      }
-
-      /* Shrink clickable regions if touch conflicts occur */
-      /* #overlay[toolbar-visible] #clickBlocks > div {
-        top: 64px;
-        bottom: 64px;
-      } */
-
-      #clickBlocks > div#toggleToolbar {
-        left: 30%;
-        right: 30%;
-        width: 40%;
-      }
-
-      #clickBlocks > div#previousImageBlock {
-        left: 0;
-        width: 30%;
-      }
-
-      #clickBlocks > div#nextImageBlock {
-        right: 0;
-        width: 30%;
-      }
-
-      @media (max-width: 600px) {
-        #clickBlocks paper-icon-button {
-          width: 40px;
-          height: 40px;
+      <style>
+        :host {
+          display: flex;
+          background: var(--sl-gallery-slideshow-background, rgba(0, 0, 0, 0.9));
+          position: fixed;
+          top: 0;
+          left: 0;
+          height: 100vh;
+          width: 100vw;
+          z-index: 1;
+          align-items: center;
+          justify-content: center;
+          overflow: hidden;
+          outline: none;
         }
 
+        :host([hidden]) {
+          display: none !important;
+        }
+
+        paper-spinner-lite {
+          position: absolute;
+          width: 42px;
+          height: 42px;
+          top: 50%;
+          left: 50%;
+          transform: translateX(-50%) translateY(-50%);
+          z-index: 1;
+          --paper-spinner-color: var(--sl-gallery-accent-color, #00AEC5);
+          --paper-spinner-stroke-width: 4px;
+        }
+
+        .image-container {
+          display: block;
+        }
+
+        .image-container iron-image {
+          width: 100vw;
+          height: 100vh;
+        }
+
+        /* Overlay Animations */
+
+        #overlay app-toolbar,
+        #overlay #clickBlocks > div,
         paper-icon-button#closeSlideshow {
-          top: 5px;
-          right: 5px;
+          will-change: transform, opacity;
+          transition: top 0.35s ease, bottom 0.35s ease, right 0.35s ease,
+                      opacity 0.35s ease, visibility 0.35s ease;
+        }
+
+        /* Overlay Toolbar */
+
+        #overlay app-toolbar {
+          z-index: 2;
+          position: fixed;
+          left: 0;
+          right: 0;
+          background-color: rgba(0, 0, 0, 0.65);
+          color: white;
+        }
+
+        #overlay app-toolbar.header {
+          top: -64px;
+          height: 64px;
+          visibility: hidden;
+        }
+
+        #overlay app-toolbar.footer {
+          height: auto;
+          min-height: 64px;
+          bottom: -64px;
+          opacity: 0;
+          visibility: hidden;
+          text-align: center;
+          --app-toolbar-font-size: 14px;
+          color: #ddd;
+        }
+
+        #overlay[toolbar-visible] app-toolbar.header {
+          top: 0;
+          visibility: visible;
+        }
+
+        #overlay[toolbar-visible] app-toolbar.footer[has-caption] {
+          bottom: 0;
+          opacity: 1;
+          visibility: visible;
+        }
+
+        /* Overlay Buttons */
+
+        /* @media (hover: hover) in CSS4 */
+        #overlay[hover-enabled] paper-icon-button:hover {
+          background: rgba(99, 99, 99, 0.3);
+          border: 1px solid rgba(88, 88, 88, 0.66);
+        }
+
+        #overlay[hover-enabled] paper-icon-button:active {
+          background: rgba(0, 0, 0, 0.3);
+          border: 0;
+        }
+
+        #overlay > paper-icon-button,
+        #overlay :not(app-toolbar) paper-icon-button {
+          color: #fff;
+          z-index: 2;
+          border-radius: 50%;
+          background: rgba(0, 0, 0, 0.3);
+          width: 48px;
+          height: 48px;
+        }
+
+        #overlay paper-icon-button#closeSlideshow {
+          position: absolute;
+          top: 8px;
+          right: 8px;
+          visibility: visible;
+        }
+
+        #overlay[toolbar-visible] paper-icon-button#closeSlideshow {
+          /* top: -48px; */
+          right: -48px;
+          visibility: hidden;
         }
 
         #clickBlocks #previousImageBlock paper-icon-button {
-          left: 5px;
+          position: absolute;
+          left: 8px;
+          top: 50%;
+          transform: translateY(-50%);
         }
 
         #clickBlocks #nextImageBlock paper-icon-button {
-          right: 5px;
+          position: absolute;
+          right: 8px;
+          top: 50%;
+          transform: translateY(-50%);
         }
-      }
-    </style>
 
-    <div id="overlay" toolbar-visible\$="[[_toolbarVisible]]" hover-enabled\$="[[!_isTouch]]">
-      <app-toolbar class="header">
-        <div main-title="">[[activeImage.title]]</div>
+        /* Overlay Click Blocks */
+
+        #overlay #clickBlocks > div {
+          z-index: 1;
+          position: fixed;
+          top: 0;
+          bottom: 0;
+        }
+
+        /* Shrink clickable regions if touch conflicts occur */
+        /* #overlay[toolbar-visible] #clickBlocks > div {
+          top: 64px;
+          bottom: 64px;
+        } */
+
+        #clickBlocks > div#toggleToolbar {
+          left: 30%;
+          right: 30%;
+          width: 40%;
+        }
+
+        #clickBlocks > div#previousImageBlock {
+          left: 0;
+          width: 30%;
+        }
+
+        #clickBlocks > div#nextImageBlock {
+          right: 0;
+          width: 30%;
+        }
+
+        @media (max-width: 600px) {
+          #clickBlocks paper-icon-button {
+            width: 40px;
+            height: 40px;
+          }
+
+          paper-icon-button#closeSlideshow {
+            top: 5px;
+            right: 5px;
+          }
+
+          #clickBlocks #previousImageBlock paper-icon-button {
+            left: 5px;
+          }
+
+          #clickBlocks #nextImageBlock paper-icon-button {
+            right: 5px;
+          }
+        }
+      </style>
+
+      <div id="overlay" toolbar-visible$="[[_toolbarVisible]]" hover-enabled$="[[!_isTouch]]">
+        <app-toolbar class="header">
+          <div main-title="">[[activeImage.title]]</div>
+          <paper-icon-button
+              id="toggleFullscreen"
+              icon="sl-gallery:fullscreen"
+              on-click="_toggleFullscreen">
+          </paper-icon-button>
+          <paper-icon-button
+              id="toolbarCloseSlideshow"
+              icon="sl-gallery:close"
+              on-click="_closeSlideshow">
+          </paper-icon-button>
+        </app-toolbar>
+
+        <app-toolbar class="footer" has-caption$="[[activeImage._hasCaption]]">
+          <div main-title="">[[activeImage.caption]]</div>
+        </app-toolbar>
+
         <paper-icon-button
-            id="toggleFullscreen"
-            icon="sl-gallery:fullscreen"
-            on-click="_toggleFullscreen">
-        </paper-icon-button>
-        <paper-icon-button
-            id="toolbarCloseSlideshow"
+            id="closeSlideshow"
             icon="sl-gallery:close"
             on-click="_closeSlideshow">
         </paper-icon-button>
-      </app-toolbar>
 
-      <app-toolbar class="footer" has-caption\$="[[activeImage._hasCaption]]">
-        <div main-title="">[[activeImage.caption]]</div>
-      </app-toolbar>
-
-      <paper-icon-button
-          id="closeSlideshow"
-          icon="sl-gallery:close"
-          on-click="_closeSlideshow">
-      </paper-icon-button>
-
-      <div id="clickBlocks">
-        <div id="toggleToolbar" on-click="_toggleToolbar"></div>
-        <div id="previousImageBlock" on-click="_previousImage">
-          <paper-icon-button icon="sl-gallery:chevron-left"></paper-icon-button>
-        </div>
-        <div id="nextImageBlock" on-click="_nextImage">
-          <paper-icon-button icon="sl-gallery:chevron-right"></paper-icon-button>
+        <div id="clickBlocks">
+          <div id="toggleToolbar" on-click="_toggleToolbar"></div>
+          <div id="previousImageBlock" on-click="_previousImage">
+            <paper-icon-button icon="sl-gallery:chevron-left"></paper-icon-button>
+          </div>
+          <div id="nextImageBlock" on-click="_nextImage">
+            <paper-icon-button icon="sl-gallery:chevron-right"></paper-icon-button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <paper-spinner-lite
-        active="[[_spinnerActive]]"
-        hidden="[[!_spinnerActive]]">
-    </paper-spinner-lite>
+      <paper-spinner-lite
+          active="[[_spinnerActive]]"
+          hidden="[[!_spinnerActive]]">
+      </paper-spinner-lite>
 
-    <div class="image-container">
-      <iron-image
-          id="image"
-          src="[[_imageSrc]]"
-          alt=""
-          preload=""
-          fade=""
-          sizing="contain"
-          placeholder="[[_imageSmall]]"
-          loaded="{{_imageLoaded}}"
-          error="{{_imageError}}">
-      </iron-image>
-    </div>
-`;
+      <div class="image-container">
+        <iron-image
+            id="image"
+            src="[[_imageSrc]]"
+            alt=""
+            preload=""
+            fade=""
+            sizing="contain"
+            placeholder="[[_imageSmall]]"
+            loaded="{{_imageLoaded}}"
+            error="{{_imageError}}">
+        </iron-image>
+      </div>
+    `;
   }
 
   static get is() { return 'sl-gallery-slideshow'; }
